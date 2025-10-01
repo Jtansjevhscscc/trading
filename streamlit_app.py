2fd2ac127a67e6119fe80623e7779162126a5fad
Jtansjevhscscc <135506555+Jtansjevhscscc@users.noreply.github.com>
2025-10-01 15:14:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ def verify_trad():
 	last_trade = historic.get(str(day), False)
 
 	if not last_trade:
-		yesterday = day - timedelta(days=window+1)
+		yesterday = day - timedelta(days=window)
 		df = yf.download("BTC-USD", start=yesterday, end=day, auto_adjust=True)
 		df["Close_log"] = np.log(df["Close"])
 		predict_next_value(df)
@@ -340,6 +340,7 @@ while True:
 	placeholder.markdown(countdown, unsafe_allow_html=True)
 
 	time.sleep(1)
+
 
 
 
